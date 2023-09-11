fa2dd69a70dc1e71a8edf89090be0a9bac4f0863
ktmurray1999 <ktmurray@mit.edu>
2023-09-11 16:33:23 -0400

    Finnished the EulerCTRNN cell class and tested it

--- src/model.py
+++ src/model.py
@@ -1,9 +1,9 @@
 from functools import partial
 from typing import Any, Optional
+import jax
 import jax.numpy as jnp
 from jax import random
-from flax import linen as nn  # Linen API
-from flax.linen.module import compact
+from flax import linen as nn
 
 class EulerCTRNNCell(nn.RNNCellBase):
     """
@@ -18,26 +18,28 @@ class EulerCTRNNCell(nn.RNNCellBase):
     features: int
     alpha: jnp.float32
     noise: jnp.float32
-    kernel_init: initializers.Initializer = nn.initializers.glorot_normal()
-    bias_init: initializers.Initializer = nn.initializers.zeros_init()
+    one: jnp.float32 = jnp.float32(1.0)
+    kernel_init: nn.initializers.Initializer = nn.initializers.glorot_normal()
+    bias_init: nn.initializers.Initializer = nn.initializers.zeros_init()
     dtype: Optional[Any] = None
     param_dtype: Any = jnp.float32
-    carry_init: initializers.Initializer = nn.initializers.zeros_init()
+    carry_init: nn.initializers.Initializer = nn.initializers.zeros_init()
 
-    @compact
+    @nn.compact
     def __call__(self, carry, inputs,):
         """
-        A call to a EulerCTRNN cell.
+        Compute the next state and output of the EulerCTRNN cell given the current state and input.
 
         Parameters:
-            carry (tuple):
-            inputs:
+            carry (tuple): A tuple containing the current hidden state (jnp.ndarray) and a JAX random key.
+            inputs (jnp.ndarray): The input tensor for the current time step.
 
         Returns:
-
+            tuple: A tuple containing two elements:
+                - A new carry tuple containing the updated hidden state (jnp.ndarray) and a new JAX random key.
+                - A tuple containing the output tensor (jnp.ndarray) and the rate tensor (jnp.ndarray).
         """
         h, key = carry
-        key, subkey = random.split(key)
         hidden_features = h.shape[-1]
 
         dense_h = partial(
@@ -57,49 +59,6 @@ class EulerCTRNNCell(nn.RNNCellBase):
             dtype=self.dtype,
             param_dtype=self.param_dtype,
         )
-
-        noise_shape = h.shape
-        noise = random.normal(subkey, noise_shape)
-
-        new_h = (float32(1.0) - self.alpha) * h + self.alpha * (dense_h(name='recurrent_kernel')(nn.activation.tanh(h)) + dense_i(name='input_kernel')(inputs) + self.noise * noise)
-        
-        return (new_h, key), nn.activation.tanh(new_h)
-
-    def initialize_carry(self, key, input_shape,):
-        """
-        Initialize the EulerCTRNN cell carry.
-
-        Parameters:
-            key (random.PRNGKey):
-            input_shape (tuple(int)):
-
-        Returns:
-
-        """
-        batch_dims = input_shape[:1]
-        mem_shape = batch_dims + (self.features,)
-        key, subkey = random.split(key)
-        h = self.carry_init(subkey, mem_shape, self.param_dtype,)
-
-        return (h, key)
-
-    @property
-    def num_feature_axes(self,):
-        return 1
-
-class EulerCTRNN(nn.Module):
-    features: int
-    alpha: jnp.float32
-    noise: jnp.float32
-    kernel_init: initializers.Initializer = nn.initializers.glorot_normal()
-    bias_init: initializers.Initializer = nn.initializers.zeros_init()
-    dtype: Optional[Any] = None
-    param_dtype: Any = jnp.float32
-
-    @compact
-    def __call__(self, x,):
-        x, key = x
-
         dense_o = partial(
             nn.Dense,
             features=1,
@@ -110,14 +69,35 @@ class EulerCTRNN(nn.Module):
             param_dtype=self.param_dtype,
         )
 
-        ctrnn = nn.RNN(EulerCTRNNCell(
-            features=self.features,
-            alpha=self.alpha,
-            noise=self.noise,
-        ))
+        noise_shape = h.shape
+        key, subkey = random.split(key)
+        noise = random.normal(subkey, noise_shape)
 
-        init_carry = ctrnn(key, x.shape)
-        rates = ctrnn.apply(init_carry, x)
+        new_h = (self.one - self.alpha) * h + self.alpha * (dense_h(name='recurrent_kernel')(nn.activation.tanh(h)) + dense_i(name='input_kernel')(inputs) + self.noise * noise)
+
+        rates = nn.activation.tanh(new_h)
         z = dense_o(name='output_kernel')(rates)
+        
+        return (new_h, key), (z, rates)
 
-        return z, rates
+    def initialize_carry(self, key, input_shape,):
+        """
+        Initialize the state (carry) of the EulerCTRNN cell.
+
+        Parameters:
+            key (random.PRNGKey): A JAX random key for initialization.
+            input_shape (tuple[int]): The shape of an input sample, not including the time dimension.
+
+        Returns:
+            tuple: A carry tuple containing the initial hidden state (jnp.ndarray) and the JAX random key.
+        """
+        batch_dims = input_shape[:1]
+        mem_shape = batch_dims + (self.features,)
+        key, subkey = random.split(key)
+        h = self.carry_init(subkey, mem_shape, self.param_dtype,)
+        
+        return h, key
+
+    @property
+    def num_feature_axes(self,):
+        return 1
