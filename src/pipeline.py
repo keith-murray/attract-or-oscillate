7039477ffcf0cf0c89e62d62ce98d6a071c02b67
ktmurray1999 <ktmurray@mit.edu>
2023-09-19 23:38:07 -0400

    added more experiment

--- src/pipeline.py
+++ src/pipeline.py
@@ -19,17 +19,18 @@ json_path = os.path.join(task_folder, "params.json")
 with open(json_path, 'r') as f:
     json_params = json.load(f)
 
-seed = json_params['seed']
-alpha = json_params['alpha']
-grok = json_params['grok']
-corrupt = json_params['corrupt']
+seed = json_params.get('seed', 0)
+alpha = json_params.get('alpha', 0.1)
+grok = json_params.get('grok', 0)
+corrupt = json_params.get('corrupt', 0)
+batch_size = json_params.get('batch_size', 108)
 
-key = random.PRNGKey(seed) # json_param seed
+key = random.PRNGKey(seed)
 
 key, subkey = random.split(key)
-set_dataset = SETDataset(subkey, 15, 5, 108)
-set_dataset.grok_SET(grok) # json_param grok
-set_dataset.corrupt_SET(corrupt) # json_param corrupt
+set_dataset = SETDataset(subkey, 30, 5, batch_size)
+set_dataset.grok_SET(grok)
+set_dataset.corrupt_SET(corrupt)
 set_dataset.print_training_testing()
 training_tf_dataset, testing_tf_dataset, grok_tf_dataset, corrupt_tf_dataset = set_dataset.tf_datasets()
 
@@ -39,8 +40,8 @@ noise = jnp.float32(0.1)
 
 ctrnn = nn.RNN(EulerCTRNNCell(features=features, alpha=alpha, noise=noise,))
 
-lr = 0.0001
-epochs = 2500
+lr = 0.001
+epochs = 500
 
 key, subkey = random.split(key)
 state = create_train_state(ctrnn, subkey, lr,)
