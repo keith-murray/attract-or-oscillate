fbdb5600eb2560e8450f8e6003b506d33f69131d
ktmurray1999 <ktmurray@mit.edu>
2024-04-18 23:20:17 -0400

    Added examples from latest experiment

--- scripts/experiments/experiment_10/experiment_10.sh
+++ scripts/experiments/experiment_10/experiment_10.sh
@@ -9,4 +9,4 @@ echo "My task ID: $LLSUB_RANK"
 echo "Number of Tasks: $LLSUB_SIZE"
 
 # Execute the Python script for this task
-python /home/gridsan/kmurray/attract-or-oscillate/src/figure_pipeline.py $LLSUB_RANK /home/gridsan/kmurray/attract-or-oscillate/results/experiment_10
+python /home/gridsan/kmurray/attract-or-oscillate/src/pipeline.py $LLSUB_RANK /home/gridsan/kmurray/attract-or-oscillate/results/experiment_10
